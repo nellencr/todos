593cc84afe673f38a6748eb15008a6eb0a66c215
Nellen <nellencr@gmail.com>
2020-05-23 23:54:59 +0200

    Updates

---
+++ precache-manifest.7607a95ff6cba80ffd2278741cf02fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ff9d0f220e9f6e48e5066655f02ac9",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "1ec2d764a59e614ae08a",
+    "url": "/todos/static/css/main.4575fc91.chunk.css"
+  },
+  {
+    "revision": "746264d4930118b45605",
+    "url": "/todos/static/js/2.b2f5ea49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.b2f5ea49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec2d764a59e614ae08a",
+    "url": "/todos/static/js/main.a5d6e3b7.chunk.js"
+  },
+  {
+    "revision": "d2ba946d5b8c7b6aef0a",
+    "url": "/todos/static/js/runtime-main.cc685ca8.js"
+  }
+]);
